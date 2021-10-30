fcc6852f0b517a9f47697bc53f92b0209524399a
Dallas Baker <dallas.baker@sheldyn.io>
2021-10-29 21:44:45 -0700

    Base Tooling :seedling:
    Updated configs and services for tooling purposes.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 const App = () => {
-  return <div>Test</div>;
+  return <div>REACT V</div>;
 };
+
 render(<App />, document.getElementById('root'));
